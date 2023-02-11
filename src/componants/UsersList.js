4e5a5977003fc54f4b465db6aa4c383816b7fcfb
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-02-11 20:13:15 +0200

    userRow no sound

--- src/componants/UsersList.js
+++ src/componants/UsersList.js
@@ -1,23 +1,16 @@
 import React from 'react'
 
-import { Howl, Howler } from 'howler';
-import hover from '../attachments/hover3.mp3'
-
 import "../Styles/UsersList.css"
 import UserRow from './UserRow'
 
 export default function UsersList({ UsersListToDisplay }) {
 
-    const sound = new Howl({
-        src: [hover]
-    })
-    Howler.volume(0.8)
     return (
         <>
             <ul className='listOfUsers'>
                 {
                     UsersListToDisplay?.map((user) => (
-                        <li key={user?._id} className='userRow'><UserRow user={user} onMouseEnter={() => sound.play()} /></li>
+                        <li key={user?._id} className='userRow'><UserRow user={user} /></li>
                     ))
                 }
             </ul>
