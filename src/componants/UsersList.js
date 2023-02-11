a1ad4f860bb2a3a608526307ad399cab1ef85261
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-02-11 19:57:40 +0200

    sound.pause

--- src/componants/UsersList.js
+++ src/componants/UsersList.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
+import { Howl, Howler } from 'howler';
+import hover from '../attachments/hover2.mp3'
+
 import "../Styles/UsersList.css"
 import UserRow from './UserRow'
 
 export default function UsersList({ UsersListToDisplay }) {
 
+    const sound = new Howl({
+        src: [hover]
+    })
+
     return (
         <>
-            <ul className='listOfUsers'>
+            <ul className='listOfUsers' onMouseEnter={() => sound.play()} onMouseLeave={() => sound.pause()}>
                 {
                     UsersListToDisplay?.map((user) => (
                         <li key={user?._id} className='userRow'><UserRow user={user} /></li>
