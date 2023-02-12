ff8c1bdad680a971490c17d8cc8f608d4e4308f1
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-02-12 18:43:38 +0200

    favorites fix

--- src/componants/PetsList.js
+++ src/componants/PetsList.js
@@ -9,7 +9,7 @@ export default function PetsList({ petListToDisplay }) {
     return (
         <ul className='listOfUserPets'>
             {
-                petListToDisplay ?
+                petListToDisplay[0] ?
                     petListToDisplay?.map((Pet) => (
                         <li key={Pet._id} ><PetCard petData={Pet} /></li>
                     ))
