179028d9d72c10ce4444a800eee52e0bac5b5cde
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-02-13 15:52:45 +0200

    only desktop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import "./Styles/Fonts/SF-Pro.ttf"
 import LandingPage from './Pages/LandingPage';
 import MainDashboard from "./Pages/MainDashboard"
+import PWBLogo from "./attachments/PWB icon2.svg"
+import PWBtext from "./attachments/Pets WithBenefits.svg"
+import imacPhoto from "./attachments/od-imac.png"
 
 import { useAuth } from './Contexts/AuthContext';
 import { PetContextProvider } from './Contexts/PetContext';
@@ -20,6 +23,16 @@ function App() {
         <PetContextProvider>
           <div className="App">
             <MainDashboard />
+            <div className='onlyDesktop'>
+              <div className='od-header'>
+                <img src={PWBLogo} alt="PWBlogo" id='od-PWBlogo'></img>
+                <img src={PWBtext} alt="PWBtext" id='od-PWBtext'></img>
+              </div>
+              <div className='od-body'>
+                <div className='od-title'>Welcome To <p>Pets With Benefits.</p>You can use our service on your desktop...</div>
+                <img src={imacPhoto} alt='imacPhoto' />
+              </div>
+            </div>
           </div>
         </PetContextProvider>
       </UserContextProvider>
